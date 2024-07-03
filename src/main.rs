47e039b535c5ec52eb5c9b054d2807db9684bc76
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-07-03 01:01:10 -0700

    basic plotting, also dealing with stupid dependency issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// hide console window on Windows in release
+use eframe::egui::{self, CentralPanel, Visuals};
+use egui_plotter::EguiBackend;
+use plotters::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        native_options,
+        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+    );
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        let context = &cc.egui_ctx;
+        context.tessellation_options_mut(|tess_options| {
+            tess_options.feathering = false;
+        });
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let root = EguiBackend::new(ui).into_drawing_area();
+            root.fill(&WHITE).unwrap();
+            let mut chart = ChartBuilder::on(&root)
+                .caption("y=x^2", ("sans-serif", 50).into_font())
+                .margin(5)
+                .x_label_area_size(30)
+                .y_label_area_size(30)
+                .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)
+                .unwrap();
+
+            chart.configure_mesh().draw().unwrap();
+
+            chart
+                .draw_series(LineSeries::new(
+                    (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+                    &RED,
+                ))
+                .unwrap()
+                .label("y = x^2")
+                .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+            chart
+                .configure_series_labels()
+                .background_style(&WHITE.mix(0.8))
+                .border_style(&BLACK)
+                .draw()
+                .unwrap();
+
+            root.present().unwrap();
+        });
+    }
 }
